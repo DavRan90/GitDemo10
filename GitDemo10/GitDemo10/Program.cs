2e2b44bd63d2e0501371fb114ca0c60c6605eee5
David Rantanen <david.rantanen@telia.com>
2024-10-23 11:01:00 +0200

    New class with method for printing text

--- GitDemo10/GitDemo10/Program.cs
+++ GitDemo10/GitDemo10/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hejsan världen!");
+            Helpers.WriteText("Test");
+            
         }
     }
 }
